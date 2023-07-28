1d1ca9eb51f4c4a0166e78ef8ef948ed5c78778d
Emad Malekpour <prodemmi@gmail.com>
2023-07-28 12:39:25 +0330

    fix posts page

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -3,16 +3,18 @@
 @endsection
 
 @section('body')
-    <h1 class="text-3xl mb-2 font-bold dark:text-white">All Posts</h1>
-    <hr>
-    <ul class="divide-y divide-gray-200 dark:divide-gray-700">
-        @forelse($posts as $post)
-            <li class="py-4">
-                <livewire:components.post :post="$post"/>
-            </li>
-        @empty
-            No Posts Found.
-        @endforelse
-    </ul>
-    {{ $posts->links() }}
+    <div>
+        <h1 class="text-3xl mb-2 font-bold dark:text-white">All Posts</h1>
+        <hr>
+        <ul class="divide-y divide-gray-200 dark:divide-gray-700">
+            @forelse($posts as $post)
+                <li class="py-4">
+                    <livewire:components.post :post="$post"/>
+                </li>
+            @empty
+                No Posts Found.
+            @endforelse
+        </ul>
+        {{ $posts->links() }}
+    </div>
 @endsection
